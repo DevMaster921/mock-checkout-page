24fc2d3d72b3c13adc3f02bcb453dd9e174fd827
DevMaster921 <webdev537@gmail.com>
2023-08-10 07:18:25 -0600

    feature: mock checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DeliveryCard from "./components/DeliveryCard";
+import PaymentCard from "./components/PaymentCard";
+import TotalCard from "./components/TotalCard";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-left-container">
+        <DeliveryCard />
+        <PaymentCard />
+      </div>
+      <div className="App-right-container">
+        <TotalCard />
+      </div>
     </div>
   );
 }
